cb3c2f62eaedcfe102134312aa147410e0a15e9b
yusuf.kenan.girgin <yusufkenan.girgin@bilgeadamboost.com>
2024-04-03 09:51:01 +0300

    a

--- 01.04.2024/js/Ts/ornekTs.js
+++
@@ -1,9 +0,0 @@
-{
-    var sayi1 = 15;
-    var sayi2 = 25;
-    var sonuc = function (sayi1, sayi2) { return sayi1 + sayi2; };
-    console.log(sonuc(sayi1, sayi2));
-    // const obj = { width: 10, height: 15 };
-    // const area = obj.width * obj.heigth;
-    // console.log(area);
-}
