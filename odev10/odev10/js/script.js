b60768caf27a6ceaac22d93ec05e64a0366c3f73
yusuf.kenan.girgin <yusufkenan.girgin@bilgeadamboost.com>
2024-04-27 10:05:03 +0300

    27.04.2024

--- odev10/odev10/js/script.js
+++ odev10/odev10/js/script.js
@@ -1,4 +1,19 @@
-function hesapla() {
+function showMenu(menuId) {
+    // Tüm menüleri gizle
+    var menus = document.querySelectorAll('.content');
+    menus.forEach(function(menu) {
+      menu.style.display = 'none';
+    });
+    
+    // Belirli menüyü göster
+    var menu = document.getElementById(menuId);
+    if (menu) {
+      menu.style.display = 'block';
+    }
+  }
+  
+
+  function hesapla() {
     // Kullanıcıdan girdileri al
     var kalinacakGun = parseInt(document.getElementById("kalinacakGun").value);
     var gunlukUcret = parseFloat(document.getElementById("gunlukUcret").value);
